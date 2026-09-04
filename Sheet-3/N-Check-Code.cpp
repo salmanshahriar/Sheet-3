c73b55f737b7cd4d126080fadb33f74befad59bd
salmanshahriar <salmanshahriar16@gmail.com>
2026-09-05 05:40:07 +0600

    feat: implement validation logic for string format in N-Check-Code.cpp

--- Sheet-3/N-Check-Code.cpp
+++ Sheet-3/N-Check-Code.cpp
@@ -38,3 +38,35 @@
 
 // The (A+1)-th character of code is '-', and the other characters are digits
 // from '0' through '9', so it follows the format.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  string s;
+  cin >> s;
+
+  if (s[a] != '-') {
+    cout << "No";
+    return 0;
+  }
+
+  for (int i = 0; i < s.size(); i++) {
+
+    if (i == a) {
+      continue;
+    }
+
+    if (s[i] < '0' || s[i] > '9') {
+      cout << "No";
+      return 0;
+    }
+  }
+
+  cout << "Yes";
+
+  return 0;
+}
